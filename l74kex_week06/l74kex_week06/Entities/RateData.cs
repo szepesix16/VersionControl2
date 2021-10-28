28e95c4776271848544ce74bca07491ed975f487
szepesix16 <xaver.szepesi@stud.uni-corvinus.hu>
2021-10-29 00:15:25 +0200

    t2

---
+++ l74kex_week06/l74kex_week06/Entities/RateData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace l74kex_week06.Entities
+{
+    class RateData
+    {
+        public DateTime Date { get; set; }
+        public string Curreny { get; set; }
+        public decimal Value { get; set; }
+    }
+}
--- l74kex_week06/l74kex_week06/Form1.cs
+++ l74kex_week06/l74kex_week06/Form1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using l74kex_week06.Entities;
+using l74kex_week06.MnbServiceReference;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +14,21 @@ namespace l74kex_week06
 {
     public partial class Form1 : Form
     {
+        BindingList<RateData> Rates = new BindingList<RateData>();
         public Form1()
         {
             InitializeComponent();
+            var mnbService = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+                var response = mnbService.GetExchangeRates(request);
+
+                var result = response.GetExchangeRatesResult;
+        }
         }
     }
-}
