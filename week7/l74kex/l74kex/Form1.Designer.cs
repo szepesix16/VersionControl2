df8119eb63b8d0c37c8fe8e3623cb8febc45bb5b
Xaver Szepesi <xaver.szepesi@stud.uni-corvinus.hu>
2021-11-12 12:48:26 +0100

    t3

--- week7/l74kex/l74kex/Form1.Designer.cs
+++ week7/l74kex/l74kex/Form1.Designer.cs
@@ -30,12 +30,35 @@ namespace l74kex
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.createTimer = new System.Windows.Forms.Timer(this.components);
+            this.conveyorTimer = new System.Windows.Forms.Timer(this.components);
+            this.mainPanel = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // mainPanel
+            // 
+            this.mainPanel.Location = new System.Drawing.Point(139, 104);
+            this.mainPanel.Name = "mainPanel";
+            this.mainPanel.Size = new System.Drawing.Size(227, 288);
+            this.mainPanel.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1120, 648);
+            this.Controls.Add(this.mainPanel);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Timer createTimer;
+        private System.Windows.Forms.Timer conveyorTimer;
+        private System.Windows.Forms.Panel mainPanel;
     }
 }
 
