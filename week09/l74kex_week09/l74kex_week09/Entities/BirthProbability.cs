0b8d7d7c1afac9cc8ffd092040611723364b2f62
Xaver Szepesi <xaver.szepesi@stud.uni-corvinus.hu>
2021-12-03 13:38:00 +0100

    t2

---
+++ week09/l74kex_week09/l74kex_week09/Entities/BirthProbability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace l74kex_week09.Entities
+{
+    public class BirthProbability
+    {
+        public int BirthYear { get; set; }
+        public int NbrOfChildren { get; set; }
+        public double P { get; set; }
+    }
+}
---
+++ week09/l74kex_week09/l74kex_week09/Entities/DeathProbability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace l74kex_week09.Entities
+{
+    class DeathProbability
+    {
+        public Gender Gender{ get; set; }
+        public int BirthYear { get; set; }
+        public double P { get; set; }
+    }
+}
