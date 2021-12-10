54151eb43f6c19b9cb81c67a01ca222a26cecbc1
Xaver Szepesi <xaver.szepesi@stud.uni-corvinus.hu>
2021-12-10 13:10:41 +0100

    t4

--- week11/l74kex_week11/l74kex_week11/Form1.cs
+++ week11/l74kex_week11/l74kex_week11/Form1.cs
@@ -35,6 +35,7 @@ namespace l74kex_week11
                 gc.AddPlayer(nbrOfSteps);
             }
             gc.Start();
+
         }
         private void Gc_GameOver(object sender)
         {
@@ -42,6 +43,26 @@ namespace l74kex_week11
             label1.Text = string.Format(
                 "{0}. generáció",
                 generation);
+            gc.ResetCurrentLevel();
+            var playerList = from p in gc.GetCurrentPlayers()
+                             orderby p.GetFitness() descending
+                             select p;
+            var topPerformers = playerList.Take(populationSize / 2).ToList();
+            foreach (var p in topPerformers)
+            {
+                var b = p.Brain.Clone();
+                if (generation % 3 == 0)
+                    gc.AddPlayer(b.ExpandBrain(nbrOfStepsIncrement));
+                else
+                    gc.AddPlayer(b);
+
+                if (generation % 3 == 0)
+                    gc.AddPlayer(b.Mutate().ExpandBrain(nbrOfStepsIncrement));
+                else
+                    gc.AddPlayer(b.Mutate());
+            }
+            gc.Start();
         }
+
     }
 }
